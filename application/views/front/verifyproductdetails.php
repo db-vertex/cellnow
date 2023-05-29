629af32cd4f58cf4332767cec2992797d2e39eaf
db-vertex <dbvertexbackend@gmail.com>
2023-05-29 12:54:14 +0530

    slider changes

--- application/views/front/verifyproductdetails.php
+++ application/views/front/verifyproductdetails.php
@@ -1751,13 +1751,11 @@ $(function() {
     this step is important if you use a fadeIn duration for the master img more than 1s, but if you use a duration less than 1s
     you don't need to make the height of the master div is static, and it is preferred to make the duration less than 1s to prevent the
     user to choose 2 images at the same time, so the implementation of the code will be faster than the user selection*/
-    $(".master").css({
-        height: $(".master img").height() + 13
-    });
+   
 
     //make the width of the thumbnails images is dynamic
     var imagesNumber        = $(".thumbnails").children().length,
-        marginBetweenImages =  1,
+        marginBetweenImages =  6,
         totalMargins        = marginBetweenImages * (imagesNumber - 1),
         imageWidth          = (100 - totalMargins) / (imagesNumber);
         
