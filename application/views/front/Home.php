fd20b69e95587674940c267ddca13db0cd62f027
db-vertex <dbvertexbackend@gmail.com>
2023-06-16 12:45:05 +0530

    changes

--- application/views/front/Home.php
+++ application/views/front/Home.php
@@ -369,8 +369,8 @@ foreach ($category as $key => $cat) {
                     </div>
                     <div class="card-body">
                         <div class="d-flex justify-content-between align-items-center">
-                            <h6 class="mb-3">
-                                <strong class="ms-2 ">$50.99</strong>
+                            <h6 style="color:#10B981" class="mb-3">
+                                <strong class="ms-2 ">₹<?php echo $pro->price; ?></strong>
                             </h6>
                         </div>
                         <div class="d-flex justify-content-between align-items-center">
