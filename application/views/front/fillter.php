dfeb35924bcd20739ac49a147bf81d2fca19a5fb
db-vertex <dbvertexbackend@gmail.com>
2023-07-26 12:44:39 +0530

    chnages

--- application/views/front/fillter.php
+++ application/views/front/fillter.php
@@ -6,24 +6,9 @@
     .cursor-center {
   margin-left:30px;
 }
-/* width */
-.scrollbar::-webkit-scrollbar {
-  width: 10px;
-}
-
-/* Track */
-.scrollbar::-webkit-scrollbar-track {
-  background: #f1f1f1; 
-}
- 
-/* Handle */
-.scrollbar::-webkit-scrollbar-thumb {
-  background: #888; 
-}
-
-/* Handle on hover */
-.scrollbar::-webkit-scrollbar-thumb:hover {
-  background: #555; 
+._1un4s {
+    max-height: 200px;
+    overflow-y: auto;
 }
 .details_img_cover {
 
@@ -1562,7 +1547,7 @@ $brands = array(
 
 <!-- Loop through the brands array and generate checkboxes with labels -->
 <div id="myDropdown">
-    <ul class="scrollbar">
+    <ul class="_1un4s">
         <?php foreach ($brands as $brand) : ?>
             <li>
                 <label>
