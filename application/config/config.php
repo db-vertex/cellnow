2d2734950210090f661b3c7d39c3643e60214e34
dbvertexphp <128798599+dbvertexphp@users.noreply.github.com>
2023-08-24 17:36:47 +0530

    changes

--- application/config/config.php
+++ application/config/config.php
@@ -26,6 +26,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  //$config['base_url'] = 'http://localhost:8000/';
 $config['base_url'] = 'https://celnow.com/';
 
+
 /*
 |--------------------------------------------------------------------------
 | Index File
