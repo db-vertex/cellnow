c688bebcb38c2e00d6b7231f7b304088bd762c70
dbvertexphp <128798599+dbvertexphp@users.noreply.github.com>
2023-08-26 10:53:25 +0530

    changes password

--- application/views/front/shop.php
+++ application/views/front/shop.php
@@ -1354,7 +1354,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLongTitle">Update Profile Picture</h5>
+                <h5 class="modal-title" id="exampleModalLongTitle">Update Shop Banner</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
